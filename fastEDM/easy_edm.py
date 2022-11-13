924f0a4fc28c39ef92710ffffdd3e10ec570a0db
Rishi D <68461494+print-hi@users.noreply.github.com>
2022-11-13 21:35:57 +1100

    Refactor easy_edm into subfunctions

--- fastEDM/easy_edm.py
+++ fastEDM/easy_edm.py
@@ -8,52 +8,94 @@ from fastEDM import edm
 
 debug = True
 
-#' easy_edm
-#'
-#' @param cause The causal time series (as a string or a vector).
-#'
-#' @param effect The effect time series (as a string or a vector).
-#'
-#' @param time For non-regularly sampled time series, the sampling times
-#' must be supplied here (as a string or a vector).
-#'
-#' @param data If a dataframe is supplied here, then the cause, effect & time
-#' arguments must be the column names of the relevant time series as strings.
-#'
-#' @param direction A string specifying whether we are checking a one
-#' directional causal effect or whether to test the reverse direction at the
-#' same time (work in progress!).
-#'
-#' @param verbosity The level of detail in the output.
-#'
-#' @param showProgressBar Whether or not to print out a progress bar during the computations.
-#'
-#' @param normalize Whether to normalize the inputs before starting EDM.
-#'
-#' @returns A Boolean indicating that evidence of causation was found.
-#' @export
-#' @example man/chicago-easy-edm-example.R
-#
+
 def easy_edm(cause, effect, time = None, data = None, direction = "oneway", 
              verbosity = 1, showProgressBar = None, normalize = True):
+    '''
+    Simplified interface for EDM function
+    ...
+    Parameters
+    ----------
+    cause : str or list  
+        The causal time series (as a string or a vector).
+    effect : str or list 
+        The effect time series (as a string or a vector).
+    time : str or list 
+        For non-regularly sampled time series, the sampling times must be supplied here.
+    data : Pandas.DataFrame or None
+        If a dataframe is supplied here, then the cause, effect & time
+        arguments must be the column names of the relevant time series as strings.
+    direction : str
+        A string specifying whether we are checking a one directional causal effect or whether 
+        to test the reverse direction at the same time (work in progress!).
+    verbosity : int
+        The level of detail in the output.
+    showProgressBar : bool
+        Whether or not to print out a progress bar during the computations.
+    normalize : bool
+        Whether to normalize the inputs before starting EDM.
+    Returns
+    ----------
+    bool: Indicator for if evidence of causation was found.
+    '''
+
+    max_theta, num_thetas, theta_reps = 5, 100, 20 # !! Parameterise these values later
+    convergence_method = "parametric"              # !! Parameterise these values later
 
     if not showProgressBar:
         showProgressBar = verbosity > 0
 
     # Convert time series to arrays (they can be supplied as columns of a dataframe).
+    t, x, y = preprocess_inputs(data, cause, effect, time, verbosity, normalize)
+
+    if (debug):
+        print(f"\n=== Finding optimal E using simplex projection.")
+        
+    # Find optimal E (embedding dimension) of the causal variable using simplex projection
+    E_best = find_embedding_dimension(t, x, verbosity, showProgressBar)
+
+    if (debug):
+        print(f"\n=== Testing for non-linearity using S-Map.")
+        
+    # Test for non-linearity using S-Map    
+    test_nonlinearity(t, x, E_best, max_theta, num_thetas, theta_reps, verbosity, showProgressBar)
+    
+    if (debug):
+        print(f"\n=== Testing for causality using CCM.")
+        
+    # Perform cross-mapping (CCM)
+    res = cross_mapping(t, x, y, E_best, verbosity, showProgressBar)
+    
+    # Test for causality using CCM
+    if convergence_method == "parametric":
+        conv_test = test_convergence_monster
+    elif convergence_method == "hypothesis":
+        conv_test = test_convergence_monster # Replace this later
+    else:
+        conv_test = test_convergence_monster # Replace this later
+    
+    outcome = conv_test(res, data, cause, effect, verbosity)
+    
+    return outcome
+
+
+def preprocess_inputs(data, cause, effect, time, verbosity, normalize):
+    '''
+    Convert time series to arrays (they can be supplied as columns of a dataframe).
+    '''
     givenTimeSeriesNames = data is not None
     if givenTimeSeriesNames:
         if verbosity > 0:
             print("Pulling the time series from the supplied dataframe.")
         if cause not in data.columns:
             print(f"{cause} is not a column in the supplied dataframe.")
-            return 1
+            exit(1)
         if effect not in data.columns:
             print(f"{effect} is not a column in the supplied dataframe.")
-            return 1
+            exit(1)
         if time is not None and time not in data.columns:
             print(f"{time} is not a column in the supplied dataframe.")
-            return 1
+            exit(1)
         x, y = data[cause], data[effect]
         t = data[time] if time else list(range(len(x)))
     else:
@@ -64,7 +106,7 @@ def easy_edm(cause, effect, time = None, data = None, direction = "oneway",
 
     if len(t) != len(x) or len(t) != len(y):
         print("Time series are not the same len .")
-        return 1
+        exit(1)
 
     if (verbosity > 0):
         print(f"Number of observations is {len(t)}")
@@ -74,12 +116,14 @@ def easy_edm(cause, effect, time = None, data = None, direction = "oneway",
             print("Normalizing the supplied time series")
         x = (x - x.mean()) / x.std()
         y = (y - y.mean()) / y.std()
+        
+    return t, x, y
 
-    # ---------------------------------------------------------------------------------------
-    # Find optimal E (embedding dimension) of the causal variable using simplex projection
-    if (debug):
-        print(f"\n=== Finding optimal E using simplex projection.")
 
+def find_embedding_dimension(t, x, verbosity, showProgressBar):
+    '''
+    Find optimal E (embedding dimension) of the causal variable using simplex projection
+    '''
     res = edm(t, x, E = list(range(3, 10 + 1)),
               verbosity = 0, showProgressBar = showProgressBar)
 
@@ -96,14 +140,14 @@ def easy_edm(cause, effect, time = None, data = None, direction = "oneway",
 
     if (verbosity > 0):
         print(f"Found optimal embedding dimension E to be {E_best}.")
+    
+    return E_best
 
-    # ---------------------------------------------------------------------------------------
-    # Test for non-linearity using S-Map    
-    if (debug):
-        print(f"\n=== Testing for non-linearity using S-Map.")
-    
-    max_theta, num_thetas, theta_reps = 5, 100, 20 # !! Parameterise these values later
-    
+
+def test_nonlinearity(t, x, E_best, max_theta, num_thetas, theta_reps, verbosity, showProgressBar):
+    '''
+    Test for non-linearity using S-Map
+    '''    
     theta_values = np.linspace(0, max_theta, 1 + num_thetas)
 
     # Calculate predictive accuracy over theta 0 to 'max_theta'
@@ -148,13 +192,13 @@ def easy_edm(cause, effect, time = None, data = None, direction = "oneway",
     if (verbosity > 0):
         print(f"Found Kolmogorov-Smirnov test statistic to be {ksStat} with p-value={ksPVal}.")
 
-    # ---------------------------------------------------------------------------------------
-    # Test for causality using CCM
+    return
 
-    if (debug):
-        print(f"\n=== Testing for causality using CCM.")
 
-    # Find the maximum library size using S-map and this E selection
+def cross_mapping(t, x, y, E_best, verbosity, showProgressBar):
+    '''
+    Find the maximum library size using S-map and this E selection
+    '''
     res = edm(t, y, E = E_best, algorithm = "smap", 
               full = True, saveManifolds = True, 
               verbosity = 0, showProgressBar = showProgressBar)
@@ -173,9 +217,14 @@ def easy_edm(cause, effect, time = None, data = None, direction = "oneway",
     libraries = [math.ceil(libraryStart + step * k) for k in range(25)]
 
     # Next run the convergent cross-mapping (CCM), using the effect to predict the cause.
-    res = edm(t, y, x, E = E_best, library = libraries, algorithm = "smap", k = np.inf, 
+    return edm(t, y, x, E = E_best, library = libraries, algorithm = "smap", k = np.inf, 
               shuffle = True, verbosity = 0, showProgressBar = showProgressBar)
 
+
+def test_convergence_monster(res, data, cause, effect, verbosity):
+    '''
+    Test for convergence using parametric test (Monster)
+    '''
     # Make some rough guesses for the Monster exponential fit coefficients
     ccmRes = res["summary"]
     firstLibrary = np.asarray(ccmRes["library"][0])
@@ -227,6 +276,7 @@ def easy_edm(cause, effect, time = None, data = None, direction = "oneway",
     else:
         causalSummary = "No evidence"
 
+    givenTimeSeriesNames = data is not None
     if givenTimeSeriesNames:
         print(f"{causalSummary} of CCM causation from {cause} to {effect} found.")
     else:
