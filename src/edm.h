e4bffc2baf25d3eac2d2112b9d4610fa0e26b302
Patrick Laub <patrick.laub@gmail.com>
2022-11-25 18:21:37 +1100

    Allow the saving of the targets of the prediction problems

--- src/edm.h
+++ src/edm.h
@@ -4,7 +4,7 @@
 
 std::vector<PredictionResult> run_tasks(const std::shared_ptr<ManifoldGenerator> generator, Options opts,
                                         const std::vector<int>& Es, const std::vector<int>& libraries, int k,
-                                        int numReps, int crossfold, bool explore, bool full, bool shuffle,
+                                        int numReps, int crossfold, bool explore, bool full, bool shuffle, bool saveFinalTargets,
                                         bool saveFinalPredictions, bool saveFinalCoPredictions, bool saveSMAPCoeffs,
                                         bool copredictMode, const std::vector<bool>& usable,
                                         const std::string& rngState, IO* io, bool keep_going(),
@@ -12,6 +12,6 @@ std::vector<PredictionResult> run_tasks(const std::shared_ptr<ManifoldGenerator>
 
 std::vector<std::future<PredictionResult>> launch_tasks(
   const std::shared_ptr<ManifoldGenerator> generator, Options opts, const std::vector<int>& Es,
-  const std::vector<int>& libraries, int k, int numReps, int crossfold, bool explore, bool full, bool shuffle,
+  const std::vector<int>& libraries, int k, int numReps, int crossfold, bool explore, bool full, bool shuffle, bool saveFinalTargets,
   bool saveFinalPredictions, bool saveFinalCoPredictions, bool saveSMAPCoeffs, bool copredictMode,
   const std::vector<bool>& usable, const std::string& rngState, IO* io, bool keep_going(), void all_tasks_finished());
--- src/python.cpp
+++ src/python.cpp
@@ -142,7 +142,7 @@ py::dict run_command(std::vector<double> t, std::vector<double> x, std::optional
                      std::optional<std::vector<int>> panel = std::nullopt, std::vector<int> es = { 2 }, int tau = 1,
                      std::vector<double> thetas = { 1.0 }, std::optional<std::vector<int>> libs = std::nullopt,
                      int k = 0, std::string algorithm = "simplex", int numReps = 1, int p = 1, int crossfold = 0,
-                     bool full = false, bool shuffle = false, bool saveFinalPredictions = false,
+                     bool full = false, bool shuffle = false, bool saveFinalTargets = false, bool saveFinalPredictions = false,
                      bool saveFinalCoPredictions = false, bool saveManifolds = false, bool saveSMAPCoeffs = false,
                      bool dt = false, bool reldt = false, double dtWeight = 0.0,
                      std::optional<std::vector<std::vector<double>>> extras = std::nullopt, bool allowMissing = false,
@@ -347,7 +347,7 @@ py::dict run_command(std::vector<double> t, std::vector<double> x, std::optional
 
 #ifndef PYODIDE
     std::vector<std::future<PredictionResult>> futures = launch_tasks(
-      genPtr, opts, Es, libraries, k, numReps, crossfold, explore, full, shuffle, saveFinalPredictions,
+      genPtr, opts, Es, libraries, k, numReps, crossfold, explore, full, shuffle, saveFinalTargets, saveFinalPredictions,
       saveFinalCoPredictions, saveSMAPCoeffs, copredictMode, usable, rngState, &io, rcpp_keep_going, nullptr);
 
     int numTasks = futures.size();
@@ -358,7 +358,7 @@ py::dict run_command(std::vector<double> t, std::vector<double> x, std::optional
     }
 #else
     std::vector<PredictionResult> results = run_tasks(
-      genPtr, opts, Es, libraries, k, numReps, crossfold, explore, full, shuffle, saveFinalPredictions,
+      genPtr, opts, Es, libraries, k, numReps, crossfold, explore, full, shuffle, saveFinalTargets, saveFinalPredictions,
       saveFinalCoPredictions, saveSMAPCoeffs, copredictMode, usable, rngState, &io, rcpp_keep_going, nullptr);
 
     int numTasks = results.size();
@@ -376,7 +376,7 @@ py::dict run_command(std::vector<double> t, std::vector<double> x, std::optional
 
     int kMin, kMax;
 
-    Eigen::MatrixXd predictions, coPredictions, coeffs;
+    Eigen::MatrixXd targets, predictions, coPredictions, coeffs;
     py::dict stats, copredStats;
     std::vector<Eigen::MatrixXd> Ms, Mps;
 
@@ -444,6 +444,11 @@ py::dict run_command(std::vector<double> t, std::vector<double> x, std::optional
           rc = pred.rc;
         }
 
+        if (pred.targets != nullptr) {
+            targets =
+              to_eigen(pred.targets.get(), pred.predictionRows.size(), 1, pred.predictionRows);
+        }
+
         if (pred.predictions != nullptr) {
           if (!pred.copredict) {
             predictions =
@@ -485,6 +490,10 @@ py::dict run_command(std::vector<double> t, std::vector<double> x, std::optional
 
     if (copredictMode) {
       res["copredStats"] = copredStats;
+    }
+
+    if (saveFinalTargets) {
+        res["targets"] = targets;
     }
 
     if (saveFinalPredictions) {
@@ -538,7 +547,7 @@ PYBIND11_MODULE(_fastEDM, m)
   m.def("run_command", run_command, py::arg("t"), py::arg("x"), "y"_a = std::nullopt, "copredict_x"_a = std::nullopt,
         "panel"_a = std::nullopt, "es"_a = std::vector<int>({ 2 }), "tau"_a = 1,
         "thetas"_a = std::vector<double>({ 1.0 }), "libs"_a = std::nullopt, "k"_a = 0, "algorithm"_a = "simplex",
-        "numReps"_a = 1, "p"_a = 1, "crossfold"_a = 0, "full"_a = false, "shuffle"_a = false,
+        "numReps"_a = 1, "p"_a = 1, "crossfold"_a = 0, "full"_a = false, "shuffle"_a = false, "saveFinalTargets"_a = false,
         "saveFinalPredictions"_a = false, "saveFinalCoPredictions"_a = false, "saveManifolds"_a = false,
         "saveSMAPCoeffs"_a = false, "dt"_a = false, "reldt"_a = false, "dtWeight"_a = 0.0, "extras"_a = std::nullopt,
         "allowMissing"_a = false, "missingDistance"_a = 0.0, "panelWeight"_a = 0.0, "panelWeights"_a = std::nullopt,
